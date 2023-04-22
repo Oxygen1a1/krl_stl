8b2b84727f6564bfd872b21e0bfb232a2f1e9a72
oxygen <304914289@qq.com>
2023-04-22 15:23:18 +0800

    string done

--- krl_stl/drv_main.cpp
+++ krl_stl/drv_main.cpp
@@ -1,5 +1,6 @@
 #include "base.h"
 #include "./vector.h"
+#include "string.h"
 
 auto unload(PDRIVER_OBJECT) -> void {
 
@@ -7,17 +8,53 @@ auto unload(PDRIVER_OBJECT) -> void {
 }
 
 
-struct _test_t {
-	int a;
-	int b;
-	int c;
-	char hha[100];
-};
+
 
 EXTERN_C auto DriverEntry(PDRIVER_OBJECT drv, PUNICODE_STRING)->NTSTATUS {
 
-	
 	drv->DriverUnload = unload;
+
+	auto s = krl_std::string();
+	auto s1 = krl_std::string("hahahahha!");
+	auto s2 = krl_std::string(s1);
+	auto s3 = krl_std::string(10, 'c');
+
+	auto s4 = s + s1;
+	auto s5 = s2 + s3;
+	//test string
+	DbgPrintEx(77, 0, "s.size() : %d\r\n", s.length());
+	DbgPrintEx(77, 0, "s1.size() : %d\r\n", s1.length());
+	DbgPrintEx(77, 0, "s2.size() : %d\r\n", s2.length());
+	DbgPrintEx(77, 0, "s3.size() : %d\r\n", s3.length());
+	DbgPrintEx(77, 0, "s4.size() : %d\r\n", s4.length());
+	DbgPrintEx(77, 0, "s5.size() : %d\r\n", s5.length());
+
+
+
+
+	s1 += s3;
+	DbgPrintEx(77, 0, "new s1.size() : %d\r\n", s1.length());
+
+	if (s1 != s2) DbgPrintEx(77, 0, "s1!=s2\r\n");
+
+	//print test
+	DbgPrintEx(77, 0, "%s\r\n", s.c_str());
+	DbgPrintEx(77, 0, "%s\r\n", s1.c_str());
+	DbgPrintEx(77, 0, "%s\r\n", s2.c_str());
+	DbgPrintEx(77, 0, "%s\r\n", s3.c_str());
+	DbgPrintEx(77, 0, "%s\r\n", s4.c_str());
+
+	//index test
+
+	DbgPrintEx(77, 0, "s1[3]=%c\r\n", s1[3]);
+
+
+	//sub str and find
+	auto s6 = krl_std::string("woshigou");
+	if (s6 == "woshigou") DbgPrintEx(77, 0, "[+]woshigou\r\n");
+
+	DbgPrintEx(77, 0, "s6.substr(2,4)=%s\r\n", s6.substr(s6.find("shi"), 4).c_str());
+	/*drv->DriverUnload = unload;
 	auto a = krl_std::vector<_test_t>(76,{55,66,77,"hahah1"});
 
 
@@ -38,7 +75,7 @@ EXTERN_C auto DriverEntry(PDRIVER_OBJECT drv, PUNICODE_STRING)->NTSTATUS {
 
 		DbgPrintEx(77, 0, "[+] %s \r\n", i->hha);
 
-	}
+	}*/
 
 	
 	//auto a = krl_std::vector<int>();
